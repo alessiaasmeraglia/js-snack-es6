9a56d2ede669c0c1c3e6f7a04536105cd64d8ce4
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-04-09 15:03:34 +0200

    Aggiunto primo esercizio

--- script.js
+++ script.js
@@ -9,3 +9,32 @@ Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
+
+const biciDaCorsa = [
+    {
+        nome: 'bici1',
+        peso: 7.8
+    },
+    {
+        nome: 'bici2',
+        peso: 7.1
+    },
+    {
+        nome: 'bici3',
+        peso: 6.9
+    },
+    {
+        nome: 'bici4',
+        peso: 7.5
+    }
+]
+
+let biciPiuLeggera = biciDaCorsa[0];
+
+for (let i = 1; i < biciDaCorsa.length; i++) {
+    if (biciDaCorsa[i].peso < biciPiuLeggera.peso) {
+        biciPiuLeggera = biciDaCorsa[i];
+    }
+}
+
+console.log("La bici con peso minore è:", biciPiuLeggera);
